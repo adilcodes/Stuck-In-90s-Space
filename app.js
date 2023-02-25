50f07e476f0d9cb734ba0ad97da22f5b7cbafba9
Adil Sheikh <ascodesofficial@gmail.com>
2023-02-25 17:00:15 +0500

    Made the countdown and timer working

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 // Making audios
 let btnClickAudio = new Audio('./audios/btnClickAudio.mp3'),
-bgAudio = new Audio('./audios/bgAudio.mp3');
+    bgAudio = new Audio('./audios/bgAudio.mp3');
 
 // Targeting DOM Elements
 let playBtn = document.querySelector("#playBtn"),
-playAndMuteBtn = document.querySelector("#play-mute"),
-popupCover = document.querySelector(".popup-cover"),
-welcomePopup = document.querySelector("#welcome-popup")
-namePopup = document.querySelector("#name-popup"),
-addNameBtn = document.querySelector("#addNameBtn"),
-playerNameInput = document.querySelector("#player-name-input"),
-greetingPlayer = document.querySelector("#greetingPlayer"),
-animatedGameHeadingCover = document.querySelector(".animated-game-name-cover"),
-levels = document.querySelectorAll(".levels button"),
-timer = document.querySelector("#timer-countdown"),
-startCountdown = document.querySelector(".start-countdown p");
+    playAndMuteBtn = document.querySelector("#play-mute"),
+    popupCover = document.querySelector(".popup-cover"),
+    welcomePopup = document.querySelector("#welcome-popup"),
+    namePopup = document.querySelector("#name-popup"),
+    addNameBtn = document.querySelector("#addNameBtn"),
+    playerNameInput = document.querySelector("#player-name-input"),
+    greetingPlayer = document.querySelector("#greetingPlayer"),
+    animatedGameHeadingCover = document.querySelector(".animated-game-name-cover"),
+    levels = document.querySelectorAll(".levels button"),
+    timer = document.querySelector("#timer-countdown"),
+    startCountdown = document.querySelector(".start-countdown p");
 
+// Global Variables
+let disabledLevels;
 
 // Event Listeners
 playBtn.addEventListener("click", () => {
@@ -35,7 +37,7 @@ playBtn.addEventListener("click", () => {
 
 playAndMuteBtn.addEventListener("click", () => {
     let playAndMuteBtnIcon = document.querySelector("#play-mute i");
-    if(playAndMuteBtnIcon.classList.contains("bi-volume-up-fill")){
+    if (playAndMuteBtnIcon.classList.contains("bi-volume-up-fill")) {
         playAndMuteBtnIcon.classList.remove("bi-volume-up-fill");
         playAndMuteBtnIcon.classList.add("bi-volume-mute-fill");
         bgAudio.volume = 0;
@@ -47,7 +49,7 @@ playAndMuteBtn.addEventListener("click", () => {
 });
 
 addNameBtn.addEventListener("click", () => {
-    if(playerNameInput.value !== ""){
+    if (playerNameInput.value !== "") {
         btnClickAudio.play();
         greetingPlayer.innerText = `Greetings Captain ${playerNameInput.value}`;
         namePopup.classList.add("popup-hidden");
@@ -55,8 +57,8 @@ addNameBtn.addEventListener("click", () => {
 
         setTimeout(() => {
             popupCover.classList.add("popup-cover-none");
-            animatedGameHeadingCover.style.display = "flex";
-            animatedGameHeadingCover.style.flexDirection = "column";
+            // animatedGameHeadingCover.style.display = "flex";
+            // animatedGameHeadingCover.style.flexDirection = "column";
             animatedGameHeadingCover.children[0].classList.add("animated-game-name-heading");
             animatedGameHeadingCover.children[1].classList.add("animated-game-guideline");
         }, 500);
@@ -71,14 +73,14 @@ levels.forEach(levelBtn => {
         let previousSelectedLvl = document.querySelector(".selectedlvl");
         previousSelectedLvl ? previousSelectedLvl.classList.remove("selectedlvl") : "";
         levelBtn.classList.add("selectedlvl");
-        let disabledLevels = document.querySelectorAll(".levels button:not(.selectedlvl)");
+        disabledLevels = document.querySelectorAll(".levels button:not(.selectedlvl)");
         disabledLevels.forEach(disabledLevel => {
             disabledLevel.setAttribute("disabled", "disabled");
         });
         animatedGameHeadingCover.style.display = "none";
         let currentSelectedlevel = document.querySelector(".selectedlvl").innerText;
         let currentSelectedlevelTime = +(currentSelectedlevel.split("").splice(0, currentSelectedlevel.length - 1).join(""));
-        if(currentSelectedlevelTime <= 9){
+        if (currentSelectedlevelTime <= 9) {
             timer.innerText = "00:0" + currentSelectedlevelTime;
         } else {
             timer.innerText = "00:" + currentSelectedlevelTime;
@@ -86,14 +88,30 @@ levels.forEach(levelBtn => {
 
         startCountdown.parentElement.style.display = "flex";
         let startCountdownextreme = 3;
-        let countdownInterval =  setInterval(() => {
-            if(startCountdownextreme > 0){
+        startCountdown.innerText = startCountdownextreme;
+        let countdownInterval = setInterval(() => {
+            if (startCountdownextreme > 0) {
                 startCountdownextreme--;
                 startCountdown.innerText = startCountdownextreme;
             } else {
+                startCountdown.parentElement.style.display = "none";
                 clearInterval(countdownInterval);
+                let timerInterval = setInterval(() => {
+                    currentSelectedlevelTime--;
+                    if (currentSelectedlevelTime >= 10) {
+                        timer.innerText = "00:" + currentSelectedlevelTime;
+                    } else if (currentSelectedlevelTime < 10 &&  currentSelectedlevelTime >= 0) {
+                        timer.innerText = "00:0" + currentSelectedlevelTime;
+                    } else {
+                        clearInterval(timerInterval);
+                        disabledLevels.forEach(disabledLevel => {
+                            disabledLevel.removeAttribute("disabled");
+                        });
+                        document.querySelector(".selectedlvl").classList.remove("selectedlvl");
+                    };
+                }, 1000);
             }
-        },1000);
+        }, 1000);
 
     });
 });
